bd28441f35d7ce8b9d38a314b4ca2f189cb937da
ttitto <ttitto@users.noreply.github.com>
2014-12-27 11:32:49 +0200

    routing for the home page added

--- Client/app/js/app.js
+++ Client/app/js/app.js
@@ -1,2 +1,8 @@
-var adsApp = angular.module('adsApp', ['ngResource'])
+var adsApp = angular.module('adsApp', ['ngResource', 'ngRoute'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/templates/ads.html'
+            })
+    }])
     .constant('baseUrl', 'http://localhost:1337/api');

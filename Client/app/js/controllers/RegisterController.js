320ede4875521e15ebf49ca28dbe7d4286237863
ttitto <ttitto@users.noreply.github.com>
2014-12-28 16:09:53 +0200

    register form validations added

---
+++ Client/app/js/controllers/RegisterController.js
@@ -0,0 +1,7 @@
+adsApp.controller('RegisterController', ['$scope', 'TownsResource',
+    function ($scope, TownsResource) {
+        $scope.emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        $scope.passwordPattern = /^[\s\S]{2,100}$/;
+
+        $scope.towns = TownsResource.all();
+    }]);
